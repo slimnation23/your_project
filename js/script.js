451749c5d9085ef8abb92e5b96d2099f9cd015d3
slimnation23 <slimnation2@gmail.com>
2020-05-06 11:05:14 +0300

    block1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
   $(".slider-blocks").slick();
+
+  let menuBtn = $(".top-nav_button"); 
+  let menu = $(".top-nav_items");
+
+  menuBtn.on("click", function (event) {
+    event.preventDefault();
+    menu.toggle("top-nav_items__active");
+  });
 });
