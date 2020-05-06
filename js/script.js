555900135c929da072db5ecdf56baee74fbbb43b
slimnation23 <slimnation2@gmail.com>
2020-05-06 20:12:41 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
   $(".slider-blocks").slick();
 
   let menuBtn = $(".top-nav_button"); 
-  let menu = $(".top-nav_items");
+  let adaptMenu = $(".adapt-menu");
   let btnScroll = $('.btn-scroll');
   let menuScroll = $('.content-block6-item_none');
   let plus = $('.fa-plus');
 
-  menuBtn.on("click", function (event) {
+  menuBtn.on('click', function(event) {
     event.preventDefault();
-    menu.toggle("top-nav_items__active");
-  });
+    adaptMenu.toggle();
+  })
+
+  // menuBtn.on("click", function (event) {
+  //   event.preventDefault();
+  //   menu.toggle("top-nav_menu__active");
+  // });
 
   btnScroll.on('click', function() {
     menuScroll.slideToggle(300);
