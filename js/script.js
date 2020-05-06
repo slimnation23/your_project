17c53951fca62035255b248b38edd31db1d4eb3e
slimnation23 <slimnation2@gmail.com>
2020-05-06 16:51:20 +0300

    block6

--- js/script.js
+++ js/script.js
@@ -3,9 +3,19 @@ $(document).ready(function () {
 
   let menuBtn = $(".top-nav_button"); 
   let menu = $(".top-nav_items");
+  let btnScroll = $('.btn-scroll');
+  let menuScroll = $('.content-block6-item_none');
+  let plus = $('.fa-plus');
 
   menuBtn.on("click", function (event) {
     event.preventDefault();
     menu.toggle("top-nav_items__active");
   });
+
+  btnScroll.on('click', function() {
+    menuScroll.slideToggle(300);
+    plus.toggle();
+  });
+
 });
+
